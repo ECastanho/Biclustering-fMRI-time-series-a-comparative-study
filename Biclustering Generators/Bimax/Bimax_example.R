d24957071e3326ca387220e8c57bf2e59a528321
ECastanho <71381940+ECastanho@users.noreply.github.com>
2020-09-18 11:50:50 +0100

    Update Bimax_example.R

--- Biclustering Generators/Bimax/Bimax_example.R
+++ Biclustering Generators/Bimax/Bimax_example.R
@@ -4,7 +4,7 @@ setwd("yourChoice")
 # Lets go pick the biclustering functions
 source("yourChoice/Bimax.R")
 
-#Loading the simtb Dataset
+#Loading the Dataset
 data = read.table(file = "yourChoice/dataset.csv", header = TRUE, sep = ',', row.names = 1)
 
 Bimax(data)
