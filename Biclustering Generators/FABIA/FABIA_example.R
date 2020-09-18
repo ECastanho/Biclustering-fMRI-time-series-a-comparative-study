20a0630d85454c7505d622846c6ade120fa8eb1f
ECastanho <71381940+ECastanho@users.noreply.github.com>
2020-09-18 11:19:23 +0100

    Update FABIA_example.R

--- Biclustering Generators/FABIA/FABIA_example.R
+++ Biclustering Generators/FABIA/FABIA_example.R
@@ -7,7 +7,7 @@ setwd("yourChoice")
 source("yourChoice/FABIA.R")
 
 #Loading the simtb Dataset
-data = read.table(file = "yourChoice/datasetS1.csv", header = TRUE, sep = ',', row.names = 1)
+data = read.table(file = "yourChoice/dataset.csv", header = TRUE, sep = ',', row.names = 1)
 
 #Running the Fabia Algorithm
 Fabia(dataset = data)  
