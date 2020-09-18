93d7bc166deb62c4b2e8cc710c35f4c2e198a86a
ECastanho <71381940+ECastanho@users.noreply.github.com>
2020-09-18 11:52:09 +0100

    Update XMotifs_example.R

--- Biclustering Generators/XMotifs/XMotifs_example.R
+++ Biclustering Generators/XMotifs/XMotifs_example.R
@@ -7,6 +7,7 @@ source("yourChoice/XMotifs.R")
 #Loading the Dataset
 data = read.table(file = "yourchoice/dataset.csv", header = TRUE, sep = ',', row.names = 1)
 
+#Running the XMotifs Algorithm
 Xmotifs(dataset = data),
 
 
