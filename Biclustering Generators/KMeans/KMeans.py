4dad1d89c9ad8d455bedf5918c4a3b91386d12f8
ECastanho <71381940+ECastanho@users.noreply.github.com>
2020-09-19 14:03:59 +0100

    Update KMeans.py

--- Biclustering Generators/KMeans/KMeans.py
+++ Biclustering Generators/KMeans/KMeans.py
@@ -33,7 +33,7 @@ import os
 from collections import defaultdict
 
 # Setting the working directory
-os.chdir('/Users/eduardocastanho/Documents/GitHub/Triclustering-algorithms-for-Spatio-Temporal-Data-Analysis/LetsApplyBiclustering/') 
+os.chdir('directory') 
 
 # Data Science Specific Libraries
 import pandas as pd 
@@ -47,11 +47,11 @@ from sklearn.cluster import SpectralClustering
 from sklearn.cluster import KMeans
 
 
-def Kmeansspectral(data, 
+def Kmeans(data, 
                        directory_out = "./",
-                       csv_name = "Clusteringspectral_Cluster_",
+                       csv_name = "Clusteringkmeans_Cluster_",
                        n_clusters=8,
-                       init='k-means++', 
+                       inite='k-means++', 
                        n_init=10,
                        max_iter=300, 
                        tol=0.0001, 
@@ -60,22 +60,12 @@ def Kmeansspectral(data,
                        random_state=None, 
                        copy_x=True, 
                        n_jobs='deprecated', 
-                       algorithm='auto'):
+                       algorithme='auto',
+                       inverted = False):
     
-    clustering = SpectralClustering(n_clusters=n_clusters,
-                                    init=init,
-                                    n_init=n_init,
-                                    max_iter=max_iter,
-                                    tol=tol,
-                                    precompute_distances=precompute_distances,
-                                    verbose=verbose,
-                                    random_state=random_state,
-                                    copy_x=copy_x,
-                                    n_jobs=n_jobs,
-                                    algorithm=algorithm
+    clustering = KMeans(n_clusters=n_clusters
                                     ).fit(data)
-    
-    
+   
     biclusters = []
     for i in range(max(clustering.labels_) + 1):
         biclusters.append(pd.DataFrame())
@@ -100,4 +90,4 @@ directory = "dataDirectory"
 
 data1 = pd.read_csv(directory, index_col=0)
 
-Clusteringspectral(data1)
+Kmeans(data1)
